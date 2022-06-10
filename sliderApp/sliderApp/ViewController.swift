465463b786d4047bdfaa10268427ba71d84ac425
Kire <kire@Kires-MacBook.local>
2022-06-09 22:19:43 -0700

    Modifying attributes of the slider object triggered by an IBAction.

--- sliderApp/sliderApp/ViewController.swift
+++ sliderApp/sliderApp/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderValue: UISlider!
     
     @IBAction func changeSlider(_ sender: UIButton) {
+        sliderValue.minimumValue = 1
+        sliderValue.maximumValue = 25
+        
+        sliderValue.value = 7
+        
+        sliderValue.minimumTrackTintColor = .red
+        sliderValue.maximumTrackTintColor = .green
+        
+        sliderValue.thumbTintColor = .black
+        
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
