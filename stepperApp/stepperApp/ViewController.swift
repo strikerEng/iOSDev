799910f6702148b3aeb5e51088c4532a3fd338bb
Kire <kire@Kires-MacBook.local>
2022-06-09 21:52:30 -0700

    Using a stepper as a way to input data, using the label as a way to display the value of the stepper, and modifying the attributes of the stepper through an IBAction triggered by a touch up inside event of the button.

--- stepperApp/stepperApp/ViewController.swift
+++ stepperApp/stepperApp/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelValue: UILabel!
+    @IBOutlet weak var stepperValue: UIStepper!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    /// Display the current value of the stepper in the labelValue
+    /// - Parameter sender: The stepper object in the view
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        labelValue.text = "\(stepperValue.value)"
+    }
+    
+    /// Change the attributes of the stepper in the view
+    /// - Parameter sender: The button in the view
+    @IBAction func changeStepper(_ sender: UIButton) {
+        stepperValue.minimumValue = -10
+        stepperValue.maximumValue = -5
+        stepperValue.stepValue = 0.5
+        stepperValue.isContinuous = true
+        stepperValue.autorepeat = true
+        stepperValue.wraps = true
+    }
 }
 
