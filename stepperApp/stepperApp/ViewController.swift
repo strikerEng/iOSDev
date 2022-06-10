961a18870f4d704625e4126b5d3ce42d25b94d55
Kire <kire@Kires-MacBook.local>
2022-06-09 21:41:02 -0700

    Example app to work with steppers as a way for users to input numeric data.

---
+++ stepperApp/stepperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stepperApp
+//
+//  Created by Kire  on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
