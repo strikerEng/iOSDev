88b71d638418f936fcbcb82c03402d48c2feb56e
Kire <kire@Kires-MacBook.local>
2022-06-09 18:36:55 -0700

    Apps working with controllers, views, and objects that get added to view.

---
+++ AddAndDeleteControllers/AddAndDeleteControllers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AddAndDeleteControllers
+//
+//  Created by Kire  on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+     In this project I learned how to remove and add a view controller.
+     After deleting a view controller, the newly added view controller must be set
+         as the initial view from the attributes inspector
+     View controllers can also be integrated into a Navigation Controller that way getting back to a previous screen can be handled automatically instead of adding ojbects to every view to control the navigation of control
+     Keep in mind that when a navigation view controller is added to the storyboard you must remove the table view controller as well
+     Also, you must identify a root view controller for the navigation controller to start in after doing the ctrl + drag 
+     */
+
+
+}
+
---
+++ ObjectApp/ObjectApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectApp
+//
+//  Created by Kire  on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
