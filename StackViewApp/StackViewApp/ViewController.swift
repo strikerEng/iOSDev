7fee4a2fd22baa1c622a11fbd17ecdab35268145
Kire <kire@Kires-MacBook.local>
2022-06-17 13:04:08 -0700

    Added a labe, button, and text view to a stack view. Also experimented with the attributes of the stack view to manipulate the appearance of the objects in the Stack view.

---
+++ StackViewApp/StackViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewApp
+//
+//  Created by Kire  on 6/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
