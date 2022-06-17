7b474c0fce6d36e8c49c16014aa0891ef85a7084
Kire <kire@Kires-MacBook.local>
2022-06-17 13:33:29 -0700

    Created a table view where each cell just showed text. The table view was had more code than necessary, specifically defining the delegate, datasource, and the table view cell.

---
+++ TableViewApp/TableViewApp/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Kire  on 6/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var petTable: UITableView!
+    
+    let petArray: [String] = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+    
+    // arbitrary constant name for a cell
+    let cellID = "cellID"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        petTable.delegate = self
+        petTable.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petArray.count
+    }
+    
+    // Called when the table view needs to put data in its rows
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create a reusable table view cell selected by its identifier
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        
+        // If the cell was not found with the about identifier, create a new table view cell
+        if (cell == nil){
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellID)
+        }
+        
+        // Set the text of the cell to the pet name at the current row index
+        cell?.textLabel?.text = petArray[indexPath.row]
+        
+        return cell!
+        
+        
+    }
+
+
+}
+
