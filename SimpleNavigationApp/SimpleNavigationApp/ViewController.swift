ed0572701b9cdb602b87948d4af72735256a5906
Kire <kire@Kires-MacBook.local>
2022-06-20 15:22:45 -0400

    Passed data to the second view controller to show a navigation title and pass a string to it's existing label.

--- SimpleNavigationApp/SimpleNavigationApp/ViewController.swift
+++ SimpleNavigationApp/SimpleNavigationApp/ViewController.swift
@@ -17,6 +17,15 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         performSegue(withIdentifier: "firstLink", sender: self)
     }
+    
+    // Do something before the segue completes
+    // This function needs to be defined in the previous view controller to pass data to the next view controller
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! SecondViewController
+        nextVC.navigationItem.title = "Second View Controller"
+        nextVC.receivedString = "Passed text"
+        
+    }
 
 
 }
