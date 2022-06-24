daf2a8334269e5de7eb5500c2c93beba0c3e29b6
Kire <kire@Kires-MacBook.local>
2022-06-23 22:28:40 -0400

    Purposely created code that would make the user interface irresponsive using thread sleep commands. While the code processes and sleeps the user can not interact with the slider which makes it seem really irresponsive.

---
+++ ThreadApp/ThreadApp/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  ThreadApp
+//
+//  Created by Kire  on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultsTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func doButton(_ sender: UIButton) {
+        let startTime = NSDate()
+        self.resultsTextView.text = ""
+        let fetchedData = self.fetchSomethingFromServer()
+        let processedData = self.processData(fetchedData)
+        let firstResult = self.calculateFirstResult(processedData)
+        let secondResult = self.calculateSecondResult(processedData)
+        let resultSummary = "First: [\(firstResult)]\n [\(secondResult)]"
+        
+        self.resultsTextView.text = resultSummary
+        let endTime = NSDate()
+        
+        print("Completed in \(endTime.timeIntervalSince(startTime as Date)) seconds")
+        
+        
+    }
+    
+    func fetchSomethingFromServer() -> String{
+        Thread.sleep(forTimeInterval: 1)
+        return "Hi, there"
+    }
+    
+    func processData(_ data: String) -> String{
+        Thread.sleep(forTimeInterval: 2)
+        return data.uppercased()
+    }
+
+    func calculateFirstResult(_ data: String) -> String{
+        Thread.sleep(forTimeInterval: 3)
+        let message = "Number of chars: \(String(data).count)"
+        return message
+    }
+    
+    func calculateSecondResult(_ data: String) -> String{
+        Thread.sleep(forTimeInterval: 4)
+        return data.replacingOccurrences(of: "E", with: "e")
+    }
+    
+    
+    
+}
+
