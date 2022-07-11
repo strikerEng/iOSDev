1fad3f0e3d3b7dff45d23b8964dff7d41e20bbbc
strikerEng <kyrheepowell@gmail.com>
2022-07-10 17:20:00 -0700

    Added functionality to drop a pin on a map view based on a long press gesture.

--- LocationApp/LocationApp/ViewController.swift
+++ LocationApp/LocationApp/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+    var locationPins = [CLLocation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,10 +36,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         // Show the devices's current location on the map view
         mapView.showsUserLocation = true
+        
+        // To drop a pin we need to detect the long press gesture
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(addPin(longGesture:)))
+        mapView.addGestureRecognizer(longPressGesture)
     
     }
     
+    // Respond to a long press in the map view by adding a pin to the location of the long press
+    @objc func addPin(longGesture: UIGestureRecognizer){
+        let touchPoint = longGesture.location(in: mapView)
+        let touchLocation = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let location = CLLocation(latitude: touchLocation.latitude, longitude: touchLocation.longitude)
+        
+        let pinToAdd = MKPointAnnotation()
+        pinToAdd.coordinate = touchLocation
+        pinToAdd.title = "\(touchLocation.latitude) \(touchLocation.longitude)"
+        
+        locationPins.append(location)
+        self.mapView.addAnnotation(pinToAdd)
+        
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // When the device's location is updated show that in the text view
         if let newLocation = locations.last{
             let latitude = "\(newLocation.coordinate.latitude)"
             let longitude = "\(newLocation.coordinate.longitude)"
@@ -47,11 +69,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        // Define the zoom area based on the device's location
         let zoomArea = MKCoordinateRegion(center: self.mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
         
         self.mapView.setRegion(zoomArea, animated: true)
     }
     
+    // Remove all pins when the Clear Pins button is pushed
+    @IBAction func clearPins(_ sender: UIButton) {
+        mapView.removeAnnotations(mapView.annotations)
+        locationPins.removeAll()
+    }
     
 
 
